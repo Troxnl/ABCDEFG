12f2b12ceb44c78fa502a328205e4cc4a3c80b30
Troxnl <108320611+Troxnl@users.noreply.github.com>
2024-01-14 15:17:51 +0100

    Update Snipe.lua

--- Snipe.lua
+++ Snipe.lua
@@ -2,7 +2,6 @@ local osclock = os.clock()
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
-
 task.wait(15)
 game.Players.LocalPlayer.PlayerScripts.Scripts.Core["Idle Tracking"].Enabled = false
 game:GetService("RunService"):Set3dRenderingEnabled(false)
@@ -15,18 +14,15 @@ local ts = game:GetService("TeleportService")
 local rs = game:GetService("ReplicatedStorage")
 local snipeNormal, signal
 local Library = require(rs:WaitForChild("Library"))
-
 if snipeNormalPets == nil then
     snipeNormalPets = false
 end
-
 local vu = game:GetService("VirtualUser")
 Players.LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    task.wait(1)
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
-
 local function processListingInfo(uid, gems, item, version, shiny, amount, boughtFrom, boughtStatus, class, failMessage, snipeNormal)
     local gemamount = Players.LocalPlayer.leaderstats["💎 Diamonds"].Value
     local snipeMessage ="||".. Players.LocalPlayer.Name .. "||"
@@ -130,7 +126,6 @@ local message1 = {
             },
         }
     }
-
     local jsonMessage = http:JSONEncode(message1)
     local success, webMessage = pcall(function()
 	http:PostAsync(weburl, jsonMessage)
@@ -148,49 +143,16 @@ local message1 = {
 end
 
 local function tryPurchase(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
-    local MAX_ATTEMPTS = 3 -- Maximum number of purchase attempts
-    local attempts = 0
-    local success = false
-
-    repeat
-        attempts = attempts + 1
-
-        signal = game:GetService("RunService").Heartbeat:Connect(function()
-            if buytimestamp < workspace:GetServerTimeNow() then
-                signal:Disconnect()
-                signal = nil
-            end
-        end)
-
-        repeat
-            task.wait()
-        until signal == nil
-
-        local boughtPet, boughtMessage = rs.Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-
-        if boughtPet then
-            success = true
-        else
-            warn("Purchase failed. Retrying...")
-
-            -- Check if the error message indicates a temporary issue
-            if string.find(boughtMessage:lower(), "cannot buy that yet") then
-                task.wait(3.4) -- Wait for a short time before retrying
-            else
-                warn("Non-retryable error: " .. boughtMessage)
-                break
-            end
+    signal = game:GetService("RunService").Heartbeat:Connect(function()
+	if buytimestamp < workspace:GetServerTimeNow() then
+	    signal:Disconnect()
+	    signal = nil
         end
-    until success or attempts >= MAX_ATTEMPTS
-
-    if success then
-        processListingInfo(uid, gems, item, version, shiny, amount, username, true, class, "Purchase successful", snipeNormal)
-    else
-        warn("Max attempts reached. Purchase failed.")
-        processListingInfo(uid, gems, item, version, shiny, amount, username, false, class, "Max attempts reached. Purchase failed.", snipeNormal)
-    end
+    end)
+    repeat task.wait() until signal == nil
+    local boughtPet, boughtMessage = rs.Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage, snipeNormal)
 end
-
 
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
         if type(message) == "table" then
@@ -336,8 +298,6 @@ if string.find(item, "Huge") and unitGems <= 2000000 then
                     elseif string.find(item, "Potion VIII") and price <= 30000 and item == "Diamonds Potion VI" then
                         coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                         return
-
-
                         -- Tools 
                     elseif item == "Golden Shovel" and unitGems <= 1000000 then
                         coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
@@ -356,7 +316,6 @@ if string.find(item, "Huge") and unitGems <= 2000000 then
             end
         end
     end)
-
 local function jumpToServer() 
     local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
     local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 50) }) 
@@ -383,13 +342,11 @@ local function jumpToServer()
     end
     ts:TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
 end
-
 if PlayerInServer < 30 then
     while task.wait(10) do
 	jumpToServer()
     end
 end
-
 for i = 1, PlayerInServer do
    for ii = 1,#alts do
         if getPlayers[i].Name == alts[ii] and alts[ii] ~= Players.LocalPlayer.Name then
@@ -399,7 +356,6 @@ for i = 1, PlayerInServer do
         end
     end
 end
-
 Players.PlayerRemoving:Connect(function(player)
     getPlayers = Players:GetPlayers()
     PlayerInServer = #getPlayers
@@ -409,7 +365,6 @@ Players.PlayerRemoving:Connect(function(player)
 	end
     end
 end) 
-
 Players.PlayerAdded:Connect(function(player)
     for i = 1,#alts do
         if player.Name == alts[i] and alts[i] ~= Players.LocalPlayer.Name then
@@ -420,9 +375,7 @@ Players.PlayerAdded:Connect(function(player)
         end
     end
 end) 
-
 local hopDelay = math.random(720, 1000)
-
 while task.wait(1) do
     if math.floor(os.clock() - osclock) >= hopDelay then
         while task.wait(10) do
